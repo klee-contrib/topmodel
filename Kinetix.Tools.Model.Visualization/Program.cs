6a85bdef155461c2dc6489f00a8e6fec00b54eb2
JabX <damien.frikha@outlook.com>
2020-01-14 01:48:21 +0100

    Génération de dot pour la visu (c'est largement mieux)

--- Kinetix.Tools.Model.Visualization/Program.cs
+++ Kinetix.Tools.Model.Visualization/Program.cs
@@ -19,12 +19,21 @@ namespace TopModel.Generator
             var classes = modelStore.GetClassesFromFile(args[0], out var classesToResolve);
 
             var sb = new StringBuilder();
+            sb.Append(@$"digraph ""{args[0].Split("\\").Last()}"" {{
+  node [fontname = ""Segoe UI"" fontsize = 8 shape = record]
+  edge [fontname = ""Segoe UI"" fontsize = 8]
+
+");
             foreach (var classe in classes)
             {
-                sb.Append("[");
+                sb.Append("  ");
                 sb.Append(classe.Name);
+                sb.Append("[");
+                sb.Append($@"tooltip = ""{classe.Comment ?? string.Empty}"" label =");
+                sb.Append(@"<<table border = ""0"" cellspacing = ""0"">");
+                sb.Append(@$"<tr><td colspan=""2""><u><b>{classe.Name}</b></u></td></tr>");
+                sb.Append(@"<tr><td colspan=""2""></td></tr>");                
 
-                var first = true;
                 foreach (var prop in classe.Properties.OfType<IFieldProperty>())
                 {
                     if (prop is AssociationProperty)
@@ -32,41 +41,33 @@ namespace TopModel.Generator
                         continue;
                     }
 
-                    if (first)
-                    {
-                        first = false;
-                        sb.Append("|");
-                    }
-                    else
-                    {
-                        sb.Append(";");
-                    }
-
                     if (prop is AliasProperty alp)
                     {
                         var alias = classesToResolve[alp].Split("|");
-                        sb.Append($"({alias[1]}).{alias[0]}");
+                        sb.Append($"<tr><td align=\"left\"><b>{alp.Prefix ?? ""}{alias[0]}{alp.Suffix ?? ""}          </b></td><td align=\"left\"><i>[{alias[1]}]     </i></td></tr>");
                     }
                     else
                     {
-                        sb.Append($"{prop.Name}{(prop.Required ? "?" : string.Empty)}: {classesToResolve[prop]}");
+                        sb.Append($"<tr><td align=\"left\"><b>{prop.Name}{(prop.Required ? "?" : string.Empty)}          </b></td><td align=\"left\"><i>{classesToResolve[prop]}     </i></td></tr>");
                     }
                 }
 
-                sb.Append("]\r\n");
+                sb.Append("</table>>]\r\n");
 
                 foreach (var prop in classe.Properties.OfType<AssociationProperty>())
                 {
-                    sb.Append($"[{classe.Name}]->{(prop.Required ? "1..1" : "0..1")}{(prop.Role != null ? $" {prop.Role}" : string.Empty)}[{classesToResolve[prop]}]\r\n");
+                    sb.Append($"  {classe.Name}->{classesToResolve[prop]}[color=\"#101088\" fontcolor=\"#101088\" arrowhead=empty headlabel=\"  {(prop.Required ? "1..1" : "0..1")}{(prop.Role != null ? $" {prop.Role}" : string.Empty)}   \"]\r\n");
                 }
 
                 foreach (var prop in classe.Properties.OfType<CompositionProperty>())
                 {
-                    sb.Append($"[{classe.Name}]{(prop.Kind == Composition.Object ? "1..1" : "0..n")}+->[{classesToResolve[prop]}]\r\n");
+                    sb.Append($"  {classe.Name}->{classesToResolve[prop]}[color=\"#101088\" fontcolor=\"#101088\" arrowhead=empty arrowtail=odiamond dir=both headlabel=\"  {(prop.Kind == Composition.Object ? "1..1" : "0..n")}  \"]\r\n");
                 }
 
                 sb.Append("\r\n");
             }
+
+            sb.Append("}");
 
             Console.WriteLine(sb.ToString());
         }
