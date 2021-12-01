f0fe0707e9785bf1ab99ff57134bd4c4b19395fe
Damien Frikha <damien.frikha@kleegroup.com>
2021-12-01 19:16:00 +0100

    Initialisation LanguageServer + extension VSCode qui affiche les erreurs

---
+++ TopModel.LanguageServer/TextDocumentSyncHandler.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server.Capabilities;
+using TopModel.Core;
+
+class TextDocumentSyncHandler : TextDocumentSyncHandlerBase
+{
+    private readonly ModelStore _modelStore;
+
+    public TextDocumentSyncHandler(ModelStore modelStore)
+    {
+        _modelStore = modelStore;
+    }
+
+    public override TextDocumentAttributes GetTextDocumentAttributes(DocumentUri uri)
+    {
+        return new TextDocumentAttributes(uri, "yaml");
+    }
+
+    public override Task<Unit> Handle(DidOpenTextDocumentParams request, CancellationToken cancellationToken)
+    {
+        _modelStore.TryApplyUpdates();
+        return Unit.Task;
+    }
+
+    public override Task<Unit> Handle(DidChangeTextDocumentParams request, CancellationToken cancellationToken)
+    {
+        _modelStore.OnModelFileChange(request.TextDocument.Uri.GetFileSystemPath(), request.ContentChanges.Single().Text);
+        return Unit.Task;
+    }
+
+    public override Task<Unit> Handle(DidSaveTextDocumentParams request, CancellationToken cancellationToken)
+    {
+        _modelStore.OnModelFileChange(request.TextDocument.Uri.GetFileSystemPath());
+        return Unit.Task;
+    }
+
+    public override Task<Unit> Handle(DidCloseTextDocumentParams request, CancellationToken cancellationToken)
+    {
+        return Unit.Task;
+    }
+
+    protected override TextDocumentSyncRegistrationOptions CreateRegistrationOptions(SynchronizationCapability capability, ClientCapabilities clientCapabilities)
+    {
+        return new TextDocumentSyncRegistrationOptions
+        {
+            DocumentSelector = DocumentSelector.ForLanguage("yaml"),
+            Change = TextDocumentSyncKind.Full,
+            Save = new SaveOptions { IncludeText = true }
+        };
+    }
+}
