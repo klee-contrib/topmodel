ac6eb3497725e9e604ea47357084f2f30f3026d4
Gildéric DERUETTE <gilderic.deruette@kleegroup.com>
2022-01-12 11:05:08 +0100

    VSCode: fix ws feat check vers
    Correction workspace multi dossiers et check version topmodel

--- TopModel.VSCode/src/extension.ts
+++ TopModel.VSCode/src/extension.ts
@@ -1,29 +1,31 @@
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 import { Trace } from 'vscode-jsonrpc';
-import { ExtensionContext, workspace, commands, window, StatusBarItem, StatusBarAlignment, Terminal } from 'vscode';
+import { ExtensionContext, workspace, commands, window, StatusBarItem, StatusBarAlignment, Terminal, Uri } from 'vscode';
 import * as fs from "fs";
 import { TopModelConfig } from './types';
 const open = require('open');
+const pjson = require('../package.json');
 
 const exec = require('child_process').exec;
 const yaml = require("js-yaml");
 
 let NEXT_TERM_ID = 1;
 let currentTerminal: Terminal;
+let lsStarted = false;
 
 export function activate(context: ExtensionContext) {
-    createStatusBar();
-    checkInstall();
-    findConfFile().then((conf) => {
-        if (conf?.configPath) {
+    if (!lsStarted) {
+        createStatusBar();
+        checkInstall();
+        findConfFile().then((conf) => {
             const config = ((conf as any).config) as TopModelConfig;
-            const configPath = (conf as any).configPath;
+            const configPath = (conf as any).file.path;
             startLanguageServer(context, configPath, config);
             registerCommands(context, configPath);
-        } else {
-            handleNoConfigFound();
-        }
-    });
+        }, error => {
+            handleError(error);
+        });
+    }
 }
 
 let topModelStatusBar: StatusBarItem;
@@ -36,10 +38,10 @@ function createStatusBar() {
     topModelStatusBar.show();
 }
 
+function execute(command: string, callback: Function) {
+    exec(command, function (error: string, stdout: string, stderr: string) { callback(stdout); });
+}
 function checkInstall() {
-    function execute(command: string, callback: Function) {
-        exec(command, function (error: string, stdout: string, stderr: string) { callback(stdout); });
-    }
     execute('echo ;%PATH%; | find /C /I "dotnet"', async (dotnetIsInstalled: string) => {
         if (dotnetIsInstalled !== '1\r\n') {
             const selection = await window.showInformationMessage('Dotnet is not installed', "Show download page");
@@ -47,16 +49,38 @@ function checkInstall() {
                 open("https://dotnet.microsoft.com/download/dotnet/6.0");
             }
         } else {
-            execute('dotnet tool list -g | find /C /I "topmodel"', async (result: string) => {
-                if (result !== '1\r\n') {
-                    const selection = await window.showInformationMessage('TopModel is not installed', "Install TopModel");
-                    if (selection === "Install TopModel") {
-                        const terminal = window.createTerminal("TopModel install");
-                        terminal.sendText("dotnet tool install --global TopModel.Generator");
-                        terminal.show();
-                    }
-                }
-            });
+            checkTopModelInsall();
+        }
+    });
+}
+
+function checkTopModelInsall() {
+    execute('dotnet tool list -g | find /C /I "topmodel"', async (result: string) => {
+        if (result !== '1\r\n') {
+            const option = "Install TopModel";
+            const selection = await window.showInformationMessage('TopModel is not installed', option);
+            if (selection === option) {
+                const terminal = window.createTerminal("TopModel install");
+                terminal.sendText("dotnet tool install --global TopModel.Generator");
+                terminal.show();
+            }
+        } else {
+            checkTopModelUpdate();
+        }
+    });
+}
+
+function checkTopModelUpdate() {
+    const modgenVersion = pjson.modgenVersion;
+    execute(`dotnet tool list -g | find /C /I "topmodel.generator      ${modgenVersion}"`, async (result: string) => {
+        if (result !== '1\r\n') {
+            const option = "Update TopModel";
+            const selection = await window.showInformationMessage('TopModel can be updated', option);
+            if (selection === option) {
+                const terminal = window.createTerminal("TopModel install");
+                terminal.sendText("dotnet tool update --global TopModel.Generator");
+                terminal.show();
+            }
         }
     });
 }
@@ -78,39 +102,37 @@ function registerCommands(context: ExtensionContext, configPath: any) {
     return NEXT_TERM_ID;
 }
 
-async function findConfFile(): Promise<{ config?: TopModelConfig, configPath?: string }> {
-    return workspace.findFiles("**/topmodel*.config").then((files) => {
-        let configs: TopModelConfig[] = [];
-        let configPaths: string[] = [];
-        files.forEach((file) => {
-            const doc = fs.readFileSync(file.path.substring(1), "utf8");
-            doc
-                .split("---")
-                .filter(e => e)
-                .map(yaml.load)
-                .map(e => e as TopModelConfig)
-                .forEach((e: TopModelConfig) => {
-                    if (e.app) {
-                        configs.push(e);
-                        configPaths.push(file.path);
-                    }
-                });
-        });
-        if (configs.length > 1) {
-            window.showErrorMessage("Plusieurs fichiers de configuration trouvés. L'extension n'a pas démarré (coming soon)");
-            return { config: undefined, configPath: undefined };
-        }
-        return { config: configs[0], configPath: configPaths[0] };
+async function findConfFile(): Promise<{ config: TopModelConfig, file: Uri }> {
+    const files = await workspace.findFiles("**/topmodel*.config");
+    let configs: { config: TopModelConfig, file: Uri }[] = files.map((file) => {
+        const doc = fs.readFileSync(file.path.substring(1), "utf8");
+        const c = doc
+            .split("---")
+            .filter(e => e)
+            .map(yaml.load)
+            .map(e => e as TopModelConfig)
+            .filter(e => e.app)
+        [0];
+        return { config: c, file };
     });
+    if (configs.length > 1) {
+        throw new TopModelException("Plusieurs fichiers de configuration trouvés. L'extension n'a pas démarré (coming soon)");
+    } else if (configs.length === 0) {
+        throw new TopModelException("Topmodel a démarré car un fichier de configuration se trouvait dans votre workspace, mais il est désormais introuvable.");
+    }
+    return configs[0];
 }
 
-function startLanguageServer(context: ExtensionContext, configPath: any, config: TopModelConfig) {
+function startLanguageServer(context: ExtensionContext, configPath: string, config: TopModelConfig) {
     // The server is implemented in node
     let serverExe = 'dotnet';
 
     const args = [context.asAbsolutePath("./language-server/TopModel.LanguageServer.dll")];
-    const configRelativePath = workspace.asRelativePath(configPath);
-    args.push(configRelativePath);
+    let configRelativePath = workspace.asRelativePath(configPath);
+    if ((workspace.workspaceFolders?.length || 0) > 1) {
+        configRelativePath = configRelativePath.split("/").splice(1).join('/');
+    }
+    args.push(configPath.substring(1));
     let serverOptions: ServerOptions = {
         run: { command: serverExe, args },
         debug: { command: serverExe, args }
@@ -140,13 +162,17 @@ function startLanguageServer(context: ExtensionContext, configPath: any, config:
     context.subscriptions.push(disposable);
 }
 function startModgen(watch: boolean, configPath: string) {
-    if(!currentTerminal) {
-        currentTerminal = window.createTerminal(`Topmodel : #${NEXT_TERM_ID++}`);
+    if (!currentTerminal || !window.terminals.includes(currentTerminal)) {
+        currentTerminal = window.createTerminal({
+            name: `Topmodel : #${NEXT_TERM_ID++}`,
+            message: "Starting modgen in a new terminal"
+        });
     }
     currentTerminal.show();
     currentTerminal.sendText(
         `modgen ${configPath}` + (watch ? " --watch" : "")
     );
+
 }
 
 function handleLsReady(config: TopModelConfig, context: ExtensionContext): void {
@@ -154,10 +180,14 @@ function handleLsReady(config: TopModelConfig, context: ExtensionContext): void 
     topModelStatusBar.tooltip = "TopModel is running for app " + config.app;
     topModelStatusBar.command = "extension.topmodel";
     context.subscriptions.push(topModelStatusBar);
+    lsStarted = true;
+}
+class TopModelException {
+    constructor(public readonly message: string) { }
 }
 
-function handleNoConfigFound(): void {
+function handleError(exception: TopModelException) {
+    window.showErrorMessage(exception.message);
     topModelStatusBar.text = "$(diff-review-close) TopModel";
     topModelStatusBar.tooltip = "TopModel is not running";
-
 }
