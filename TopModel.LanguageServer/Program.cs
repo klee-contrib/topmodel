abdc187246cd89c26309f9a87e0132a537a0a8ab
Gildéric DERUETTE <gilderic.deruette@kleegroup.com>
2022-01-21 19:46:09 +0100

    fix(LS): Tri des uses
    Tri les imports
    
    Fix: #11

--- TopModel.LanguageServer/Program.cs
+++ TopModel.LanguageServer/Program.cs
@@ -30,6 +30,7 @@ var server = await LanguageServer.From(options =>
         .WithHandler<DefinitionHandler>()
         .WithHandler<CompletionHandler>()
         .WithHandler<WorkspaceSymbolHandler>()
+        .WithHandler<DocumentFormattingHandler>()
         .OnInitialize((server, _, __) =>
         {
             server.Services.GetRequiredService<ModelStore>().LoadFromConfig();
