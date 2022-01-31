2aa38b0bdc93031ce64cc4b8adec187261017279
Damien Frikha <damien.frikha@outlook.com>
2022-01-31 21:21:22 +0100

    [ModelGenerator] Version initiale générateur depuis OpenAPI/Swagger

---
+++ TopModel.ModelGenerator/OpenApi/OpenApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using SharpYaml.Serialization;
+
+namespace TopModel.ModelGenerator.OpenApi;
+
+public class OpenApiConfig
+{
+    [YamlMember("domains")]
+    public Dictionary<string, string> Domains { get; set; } = new();
+
+    [YamlMember("sources")]
+    public List<OpenApiSource> Sources { get; set; } = new();
+}
+
+public class OpenApiSource
+{
+    [YamlMember("url")]
+    public string? Url { get; set; }
+
+    [YamlMember("login")]
+    public string? Login { get; set; }
+}
