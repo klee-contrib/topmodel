f322f4e4979f3ec29db902ad3837daa7e025f8af
Damien <damien.frikha@kleegroup.com>
2022-04-15 21:35:48 +0200

    Implémentation C# d'un décorateur

--- TopModel.Core/Model/Types/CSharpDecorator.cs
+++ TopModel.Core/Model/Types/CSharpDecorator.cs
@@ -4,7 +4,7 @@ public class CSharpDecorator
 {
     public string? Extends { get; set; }
 
-    public string? Implements { get; set; }
+    public IList<string>? Implements { get; set; }
 
     public IList<string> Annotations { get; set; } = new List<string>();
 
