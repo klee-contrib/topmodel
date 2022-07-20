c99818ecbd96c854c707a391bf0bb569a138e413
Damien <damien.frikha@kleegroup.com>
2022-07-20 21:44:11 +0200

    Rename qui marche de partout (+ pour les décorateurs)

--- TopModel.LanguageServer/ReferencesHandler.cs
+++ TopModel.LanguageServer/ReferencesHandler.cs
@@ -3,7 +3,6 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using TopModel.Core;
-using TopModel.Core.FileModel;
 
 class ReferencesHandler : ReferencesHandlerBase
 {
@@ -21,36 +20,7 @@ class ReferencesHandler : ReferencesHandlerBase
         var file = _modelStore.Files.SingleOrDefault(f => _facade.GetFilePath(f) == request.TextDocument.Uri.GetFileSystemPath());
         if (file != null)
         {
-            object? objet = null;
-
-            var matchedReference = file.References.Keys.SingleOrDefault(reference =>
-                reference.Start.Line - 1 <= request.Position.Line && request.Position.Line <= reference.End.Line - 1
-                && reference.Start.Column - 1 <= request.Position.Character && request.Position.Character <= reference.End.Column - 1);
-
-            if (matchedReference != null)
-            {
-                objet = file.References[matchedReference];
-            }
-            else
-            {
-                objet =
-                    (object?)file.Classes.SingleOrDefault(c => c.Name.GetLocation()!.Start.Line - 1 == request.Position.Line || c.GetLocation()!.Start.Line - 1 == request.Position.Line)
-                    ?? (object?)file.Domains.SingleOrDefault(d => d.Name.GetLocation()!.Start.Line - 1 == request.Position.Line || d.GetLocation()!.Start.Line - 1 == request.Position.Line)
-                    ?? file.Decorators.SingleOrDefault(d => d.Name.GetLocation()!.Start.Line - 1 == request.Position.Line || d.GetLocation()!.Start.Line - 1 == request.Position.Line);
-            }
-
-            var references = objet switch
-            {
-                Class classe => new[] { (Reference: classe.Name.GetLocation()!, File: classe.GetFile()!) }
-                    .Concat(_modelStore.GetClassReferences(classe).Select(c => (Reference: (Reference)c.Reference, c.File))),
-                Domain domain => new[] { (Reference: domain.Name.GetLocation()!, File: domain.GetFile()!) }
-                    .Concat(_modelStore.GetDomainReferences(domain).Select(d => (Reference: (Reference)d.Reference, d.File))),
-                Decorator decorator => new[] { (Reference: decorator.Name.GetLocation()!, File: decorator.GetFile()!) }
-                    .Concat(_modelStore.GetDecoratorReferences(decorator).Select(d => (Reference: (Reference)d.Reference, d.File))),
-                _ => null
-            };
-
-
+            var references = _modelStore.GetReferencesForPositionInFile(request.Position, file);
             if (references != null)
             {
                 return Task.FromResult(new LocationContainer(
--- TopModel.LanguageServer/RenameHandler.cs
+++ TopModel.LanguageServer/RenameHandler.cs
@@ -3,7 +3,6 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using TopModel.Core;
-using TopModel.Core.FileModel;
 
 class RenameHandler : RenameHandlerBase
 {
@@ -21,45 +20,13 @@ class RenameHandler : RenameHandlerBase
         var file = _modelStore.Files.SingleOrDefault(f => _facade.GetFilePath(f) == request.TextDocument.Uri.GetFileSystemPath());
         if (file != null)
         {
-            IEnumerable<(Reference Reference, ModelFile File)>? references = null;
-            LocatedString? name = null;
-
-            var clazz = file.Classes.SingleOrDefault(c =>
-                c.Name.GetLocation()!.Start.Line - 1 == request.Position.Line
-                || c.GetLocation()!.Start.Line - 1 == request.Position.Line);
-
-            if (clazz != null)
-            {
-                name = clazz.Name;
-                references = _modelStore.GetClassReferences(clazz).Select(r => ((Reference)r.Reference, r.File));
-            }
-            else
-            {
-                var domain = file.Domains.SingleOrDefault(d =>
-                    d.Name.GetLocation()!.Start.Line - 1 == request.Position.Line
-                    || d.GetLocation()!.Start.Line - 1 == request.Position.Line);
-
-                if (domain != null)
-                {
-                    name = domain.Name;
-                    references = _modelStore.GetDomainReferences(domain).Select(r => ((Reference)r.Reference, r.File));
-                }
-            };
-
+            var references = _modelStore.GetReferencesForPositionInFile(request.Position, file);
             if (references != null)
             {
                 return Task.FromResult<WorkspaceEdit?>(new WorkspaceEdit()
                 {
                     Changes = references
                         .Select(r => new Location { Uri = new Uri(_facade.GetFilePath(r.File)), Range = r.Reference.ToRange()! })
-                        .Concat(new List<Location>
-                        {
-                            new()
-                            {
-                                Uri = new Uri(_facade.GetFilePath(file)),
-                                Range = name.GetLocation()!.ToRange()!
-                            }
-                        })
                         .Select(c => new
                         {
                             c.Uri,
