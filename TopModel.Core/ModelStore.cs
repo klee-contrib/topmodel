a44ceafef7269a949bcc2721bd6d95c808c9332c
Damien <damien.frikha@kleegroup.com>
2022-12-16 12:12:36 +0100

    Erreur de nom d'endpoint déjà utilisé sur les fichiers de même nom/module aussi

--- TopModel.Core/ModelStore.cs
+++ TopModel.Core/ModelStore.cs
@@ -1187,11 +1187,6 @@ public class ModelStore
             }
         }
 
-        foreach (var endpoint in fileEndpoints.Where((e, i) => modelFile.Endpoints.Where((p, j) => p.Name == e.Name && j < i).Any()))
-        {
-            yield return new ModelError(modelFile, $"Le nom '{endpoint.Name}' est déjà utilisé.", endpoint.Name.GetLocation()) { IsError = true, ModelErrorType = ModelErrorType.TMD0003 };
-        }
-
         foreach (var use in modelFile.UselessImports.Where(u => dependencies.Any(d => d.Name == u.ReferenceName)))
         {
             yield return new ModelError(modelFile, $"L'import '{use.ReferenceName}' n'est pas utilisé.", use) { IsError = false, ModelErrorType = ModelErrorType.TMD9001 };
@@ -1227,17 +1222,27 @@ public class ModelStore
             yield return new ModelError(decorator, $"Le décorateur '{decorator.Name}' n'est pas utilisé.") { IsError = false, ModelErrorType = ModelErrorType.TMD9005 };
         }
 
-        foreach (var files in Files.GroupBy(file => new { file.Options.Endpoints.FileName, file.Module }).Where(files => files.Select(file => file.Options.Endpoints.Prefix).Distinct().Count() > 1))
+        foreach (var files in Files.GroupBy(file => new { file.Options.Endpoints.FileName, file.Module }))
         {
-            foreach (var file in files)
+            var endpoints = files.SelectMany(f => f.Endpoints.Where(c => !f.ResolvedAliases.Contains(c)));
+
+            foreach (var endpoint in endpoints.Where((e, i) => files.SelectMany(f => f.Endpoints).Where((p, j) => p.Name == e.Name && j < i).Any()))
             {
-                if (file.Options.Endpoints.Prefix != null)
+                yield return new ModelError(endpoint, $"Le nom '{endpoint.Name}' est déjà utilisé.", endpoint.Name.GetLocation()) { IsError = true, ModelErrorType = ModelErrorType.TMD0003 };
+            }
+
+            if (files.Select(file => file.Options.Endpoints.Prefix).Distinct().Count() > 1)
+            {
+                foreach (var file in files)
                 {
-                    yield return new ModelError(file, $"Le préfixe d'endpoint '{file.Options.Endpoints.Prefix}' doit être identique à celui de tous les fichiers de même nom et de même module.", file.Options.Endpoints.Prefix?.GetLocation()) { ModelErrorType = ModelErrorType.TMD1021 };
-                }
-                else
-                {
-                    yield return new ModelError(file, $"Le fichier ne définit pas de préfixe d'endpoint alors que d'autres fichiers de même nom et de même module le font.") { ModelErrorType = ModelErrorType.TMD1021 };
+                    if (file.Options.Endpoints.Prefix != null)
+                    {
+                        yield return new ModelError(file, $"Le préfixe d'endpoint '{file.Options.Endpoints.Prefix}' doit être identique à celui de tous les fichiers de même nom et de même module.", file.Options.Endpoints.Prefix?.GetLocation()) { ModelErrorType = ModelErrorType.TMD1021 };
+                    }
+                    else
+                    {
+                        yield return new ModelError(file, $"Le fichier ne définit pas de préfixe d'endpoint alors que d'autres fichiers de même nom et de même module le font.") { ModelErrorType = ModelErrorType.TMD1021 };
+                    }
                 }
             }
         }
