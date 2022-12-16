861842872cac89eb6a33cb1e694745b735eff41c
Damien <damien.frikha@kleegroup.com>
2022-12-16 11:58:12 +0100

    Autres fixes de plantages potentiels sur le LS

--- TopModel.LanguageServer/DocumentSymbolHandler.cs
+++ TopModel.LanguageServer/DocumentSymbolHandler.cs
@@ -1,7 +1,7 @@
 using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
-using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using TopModel.Core;
 
 namespace TopModel.LanguageServer;
@@ -22,68 +22,71 @@ public class DocumentSymbolHandler : DocumentSymbolHandlerBase
     public override Task<SymbolInformationOrDocumentSymbolContainer> Handle(DocumentSymbolParams request, CancellationToken cancellationToken)
     {
 
-        var file = _modelStore.Files.Where(f => _facade.GetFilePath(f) == request.TextDocument.Uri).SingleOrDefault();
+        var file = _modelStore.Files.SingleOrDefault(f => _facade.GetFilePath(f) == request.TextDocument.Uri);
         if (file == null)
         {
-            return Task.FromResult<SymbolInformationOrDocumentSymbolContainer>(new SymbolInformationOrDocumentSymbolContainer());
+            return Task.FromResult(new SymbolInformationOrDocumentSymbolContainer());
         }
 
-        return Task.FromResult<SymbolInformationOrDocumentSymbolContainer>(new SymbolInformationOrDocumentSymbolContainer(
+        return Task.FromResult(new SymbolInformationOrDocumentSymbolContainer(
             file.Classes.Select(c =>
-    {
-        return new SymbolInformation
-        {
-            Deprecated = false,
-            Kind = SymbolKind.Class,
-            Name = c.Name,
-            Location = new Location
             {
-                Range = c.Name.GetLocation()!.ToRange()!,
-                Uri = request.TextDocument.Uri
-            }
-        };
-    })
-    .Concat(file.Endpoints.Select(e =>
-        {
-            return new SymbolInformation
-            {
-                Deprecated = false,
-                Kind = SymbolKind.Method,
-                Name = e.Name,
-                Location = new Location
+                return new SymbolInformation
                 {
-                    Range = e.Name.GetLocation()!.ToRange()!,
-                    Uri = request.TextDocument.Uri
-                }
-            };
-        })).Concat(file.Domains.Select(d =>
-        {
-            return new SymbolInformation
+                    Deprecated = false,
+                    Kind = SymbolKind.Class,
+                    Name = c.Name,
+                    Location = new Location
+                    {
+                        Range = c.Name.GetLocation()?.ToRange()!,
+                        Uri = request.TextDocument.Uri
+                    }
+                };
+            })
+            .Concat(file.Endpoints.Select(e =>
             {
-                Deprecated = false,
-                Kind = SymbolKind.Struct,
-                Name = d.Name,
-                Location = new Location
+                return new SymbolInformation
                 {
-                    Range = d.GetLocation().ToRange()!,
-                    Uri = request.TextDocument.Uri
-                }
-            };
-        })).Concat(file.Decorators.Select(d =>
-        {
-            return new SymbolInformation
+                    Deprecated = false,
+                    Kind = SymbolKind.Method,
+                    Name = e.Name,
+                    Location = new Location
+                    {
+                        Range = e.Name.GetLocation()?.ToRange()!,
+                        Uri = request.TextDocument.Uri
+                    }
+                };
+            }))
+            .Concat(file.Domains.Select(d =>
             {
-                Deprecated = false,
-                Kind = SymbolKind.Interface,
-                Name = d.Name,
-                Location = new Location
+                return new SymbolInformation
                 {
-                    Range = d.GetLocation().ToRange()!,
-                    Uri = request.TextDocument.Uri
-                }
-            };
-        }))
-    .Select(t => new SymbolInformationOrDocumentSymbol(t)).ToList()));
+                    Deprecated = false,
+                    Kind = SymbolKind.Struct,
+                    Name = d.Name,
+                    Location = new Location
+                    {
+                        Range = d.GetLocation()?.ToRange()!,
+                        Uri = request.TextDocument.Uri
+                    }
+                };
+            }))
+            .Concat(file.Decorators.Select(d =>
+            {
+                return new SymbolInformation
+                {
+                    Deprecated = false,
+                    Kind = SymbolKind.Interface,
+                    Name = d.Name,
+                    Location = new Location
+                    {
+                        Range = d.GetLocation()?.ToRange()!,
+                        Uri = request.TextDocument.Uri
+                    }
+                };
+            }))
+            .Where(d => d.Location.Range != null)
+            .Select(t => new SymbolInformationOrDocumentSymbol(t))));
     }
 
     protected override DocumentSymbolRegistrationOptions CreateRegistrationOptions(DocumentSymbolCapability capability, ClientCapabilities clientCapabilities)
