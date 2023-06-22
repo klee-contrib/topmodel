cc7fc628c452df224cbec1e910f066b92997e288
Damien <damien.frikha@kleegroup.com>
2023-06-22 20:51:10 +0200

    Modélisation DataFlow

---
+++ TopModel.Core/Model/DataFlow.cs
@@ -0,0 +1,32 @@
+﻿using TopModel.Core.FileModel;
+
+namespace TopModel.Core;
+
+public class DataFlow
+{
+#nullable disable
+    public LocatedString Name { get; set; }
+
+    public string Target { get; set; }
+
+    public Class Class { get; set; }
+
+    public ClassReference ClassReference { get; set; }
+
+    public DataFlowType Type { get; set; }
+#nullable enable
+
+    public List<DataFlow> DependsOn { get; set; } = new();
+
+    public List<Reference> DependsOnReference { get; set; } = new();
+
+    public bool PostQuery { get; set; }
+
+    public bool PreQuery { get; set; }
+
+    public IFieldProperty? ActiveProperty { get; set; }
+
+    public Reference? ActivePropertyReference { get; set; }
+
+    public List<DataFlowSource> Sources { get; set; } = new();
+}
---
+++ TopModel.Core/Model/DataFlowSource.cs
@@ -0,0 +1,22 @@
+﻿using TopModel.Core.FileModel;
+
+namespace TopModel.Core;
+
+#nullable disable
+
+public class DataFlowSource
+{
+    public string Source { get; set; }
+
+    public Class Class { get; set; }
+
+    public ClassReference ClassReference { get; set; }
+
+    public DataFlowSourceMode Mode { get; set; }
+
+    public List<IFieldProperty> JoinProperties { get; set; } = new();
+
+    public List<Reference> JoinPropertyReferences { get; set; } = new();
+
+    public bool InnerJoin { get; set; }
+}
