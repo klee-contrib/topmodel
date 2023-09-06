5340116fde1da71b6719e09169a55eb4e98c9d02
Damien <damien.frikha@kleegroup.com>
2023-09-06 19:16:00 +0200

    `ignoreDefaultValues` + gestion standard des (default)Values en SQL

--- TopModel.Generator.Sql/SqlConfig.cs
+++ TopModel.Generator.Sql/SqlConfig.cs
@@ -18,22 +18,30 @@ public class SqlConfig : GeneratorConfigBase
     public SsdtConfig? Ssdt { get; set; }
 
     /// <summary>
+    /// Désactive la génération des valeurs par défaut des propriétés dans les classes et endpoints générés avec cette configuration.
+    /// </summary>
+    public override bool IgnoreDefaultValues { get; set; } = true;
+
+    /// <summary>
     /// SGBD cible ("sqlserver" ou "postgres").
     /// </summary>
     public TargetDBMS TargetDBMS { get; set; } = TargetDBMS.Postgre;
+
+    protected override bool UseNamedEnums => false;
 
     public override bool CanClassUseEnums(Class classe, IEnumerable<Class>? availableClasses = null, IFieldProperty? prop = null)
     {
         return false;
     }
 
-    public bool ShouldQuoteValue(IFieldProperty prop)
+    public override bool ShouldQuoteValue(IFieldProperty prop)
     {
         var type = GetType(prop);
         return (type ?? string.Empty).Contains("varchar")
             || type == "text"
             || type == "uniqueidentifier"
             || type == "uuid"
+            || type == "bit"
             || (type ?? string.Empty).Contains("date")
             || (type ?? string.Empty).Contains("time");
     }
@@ -41,5 +49,10 @@ public class SqlConfig : GeneratorConfigBase
     protected override string GetEnumType(string className, string propName, bool isPrimaryKeyDef = false)
     {
         throw new NotImplementedException();
+    }
+
+    protected override string QuoteValue(string value)
+    {
+        return $@"{(TargetDBMS == TargetDBMS.Sqlserver ? "N" : string.Empty)}'{value.Replace("'", "''")}'";
     }
 }
