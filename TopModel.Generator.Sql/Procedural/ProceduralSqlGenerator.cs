c857b684ad23cf22f227370f7ff983a13c7860de
Gildéric DERUETTE <gilderic.deruette@kleegroup.com>
2024-03-12 18:12:59 +0100

    [i18n] Gérer les traductions de listes de référence en SQL (et C#) Fixes #174

--- TopModel.Generator.Sql/Procedural/ProceduralSqlGenerator.cs
+++ TopModel.Generator.Sql/Procedural/ProceduralSqlGenerator.cs
@@ -10,12 +10,15 @@ public class ProceduralSqlGenerator : GeneratorBase<SqlConfig>
 {
     private readonly ILogger<ProceduralSqlGenerator> _logger;
 
+    private ModelConfig _modelConfig;
+
     private AbstractSchemaGenerator? _schemaGenerator;
 
-    public ProceduralSqlGenerator(ILogger<ProceduralSqlGenerator> logger)
+    public ProceduralSqlGenerator(ILogger<ProceduralSqlGenerator> logger, ModelConfig modelConfig)
         : base(logger)
     {
         _logger = logger;
+        _modelConfig = modelConfig;
     }
 
     public override string Name => "ProceduralSqlGen";
@@ -36,8 +39,8 @@ public class ProceduralSqlGenerator : GeneratorBase<SqlConfig>
         get
         {
             _schemaGenerator ??= Config.TargetDBMS == TargetDBMS.Postgre
-                ? new PostgreSchemaGenerator(Config, _logger)
-                : new SqlServerSchemaGenerator(Config, _logger);
+                ? new PostgreSchemaGenerator(Config, _logger, _modelConfig)
+                : new SqlServerSchemaGenerator(Config, _logger, _modelConfig);
 
             return _schemaGenerator;
         }
