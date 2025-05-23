6d2c569ef3f987c9ef1cebf85d5b499ddc842968
Gildéric <gilderic.deruette@kleegroup.com>
2025-05-23 14:45:54 +0200

    [Core] Fix vérification de l'unicité du nom de propriété du endpoint

--- TopModel.Core/Resolvers/EndpointResolver.cs
+++ TopModel.Core/Resolvers/EndpointResolver.cs
@@ -13,7 +13,7 @@ internal class EndpointResolver(ModelFile modelFile)
     {
         foreach (var endpoint in modelFile.Endpoints)
         {
-            foreach (var property in endpoint.Params.Where((e, i) => endpoint.Params.Where((p, j) => p.Name == e.Name && j < i).Any()))
+            foreach (var property in endpoint.Params.Where((e, i) => endpoint.Params.Where((p, j) => p.GetParamName() == e.GetParamName() && j < i).Any()))
             {
                 yield return new ModelError(modelFile, $"Le nom '{property.Name}' est déjà utilisé.", property.Decorator is not null ? endpoint.DecoratorReferences.FirstOrDefault(dr => dr.ReferenceName == property.Decorator.Name) : property.GetLocation())
                 {
