f68b9fbcd95c1feb94f62a86332267f0675a82ce
Gildéric <gilderic.deruette@kleegroup.com>
2025-11-07 09:54:23 +0100

    Revert "[AngularApiClient] observe: "response" si responseType: "blob" (fix #508)"
    This reverts commit 343e7d24ab02baab73a08a331e2812a41d5c826d.

--- samples/output/angular/src/appgenerated/api/securite/utilisateur/utilisateur.service.ts
+++ samples/output/angular/src/appgenerated/api/securite/utilisateur/utilisateur.service.ts
@@ -2,303 +2,130 @@
 //// ATTENTION CE FICHIER EST GENERE AUTOMATIQUEMENT !
 ////
 
-import {
-  HttpClient,
-  HttpContext,
-  HttpHeaders,
-  HttpParams,
-  HttpResponse,
-} from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { TypeUtilisateurCode } from '../../../model/securite/utilisateur/references';
-import { UtilisateurItem } from '../../../model/securite/utilisateur/utilisateur-item';
-import { UtilisateurRead } from '../../../model/securite/utilisateur/utilisateur-read';
-import { UtilisateurWrite } from '../../../model/securite/utilisateur/utilisateur-write';
+
+import { HttpClient, HttpContext, HttpHeaders, HttpParams } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { TypeUtilisateurCode } from "../../../model/securite/utilisateur/references";
+import { UtilisateurItem } from "../../../model/securite/utilisateur/utilisateur-item";
+import { UtilisateurRead } from "../../../model/securite/utilisateur/utilisateur-read";
+import { UtilisateurWrite } from "../../../model/securite/utilisateur/utilisateur-write";
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root'
 })
 export class UtilisateurService {
-  private readonly http = inject(HttpClient);
 
-  /**
-   * @description Ajoute un utilisateur
-   * @param utilisateur Utilisateur à sauvegarder
-   * @returns Utilisateur sauvegardé
-   */
-  addUtilisateur(
-    utilisateur: UtilisateurWrite,
-    options: {
-      headers?: HttpHeaders | { [header: string]: string | string[] };
-      context?: HttpContext;
-      params?:
-        | HttpParams
-        | {
-            [param: string]:
-              | string
-              | number
-              | boolean
-              | ReadonlyArray<string | number | boolean>;
-          };
-      withCredentials?: boolean;
-      reportProgress?: boolean;
-      transferCache?: { includeHeaders?: string[] } | boolean;
-    } = {}
-  ): Observable<UtilisateurRead> {
-    return this.http.post<UtilisateurRead>(`/api/utilisateurs`, utilisateur, {
-      observe: 'body',
-      ...options,
-    });
-  }
+    private readonly http = inject(HttpClient);
 
-  /**
-   * @description Supprime un utilisateur
-   * @param utiId Id de l'utilisateur
-   */
-  deleteUtilisateur(
-    utiId: number,
-    options: {
-      headers?: HttpHeaders | { [header: string]: string | string[] };
-      context?: HttpContext;
-      params?:
-        | HttpParams
-        | {
-            [param: string]:
-              | string
-              | number
-              | boolean
-              | ReadonlyArray<string | number | boolean>;
-          };
-      withCredentials?: boolean;
-      reportProgress?: boolean;
-      transferCache?: { includeHeaders?: string[] } | boolean;
-    } = {}
-  ): Observable<void> {
-    return this.http.delete<void>(`/api/utilisateurs/${utiId}`, {
-      observe: 'body',
-      ...options,
-    });
-  }
-
-  /**
-   * @description Download de la photo d'un utilisateur
-   * @param utiId Id de l'utilisateur
-   * @returns Fichier de la photo
-   */
-  downloadPicture(
-    utiId: number,
-    options: {
-      headers?: HttpHeaders | { [header: string]: string | string[] };
-      context?: HttpContext;
-      params?:
-        | HttpParams
-        | {
-            [param: string]:
-              | string
-              | number
-              | boolean
-              | ReadonlyArray<string | number | boolean>;
-          };
-      withCredentials?: boolean;
-      reportProgress?: boolean;
-      transferCache?: { includeHeaders?: string[] } | boolean;
-    } = {}
-  ): Observable<HttpResponse<Blob>> {
-    return this.http.get(`/api/utilisateurs/${utiId}/picture`, {
-      responseType: 'blob',
-      observe: 'response',
-      ...options,
-    });
-  }
-
-  /**
-   * @description Charge le détail d'un utilisateur
-   * @param utiId Id de l'utilisateur
-   * @returns Le détail de l'utilisateur
-   */
-  getUtilisateur(
-    utiId: number,
-    options: {
-      headers?: HttpHeaders | { [header: string]: string | string[] };
-      context?: HttpContext;
-      params?:
-        | HttpParams
-        | {
-            [param: string]:
-              | string
-              | number
-              | boolean
-              | ReadonlyArray<string | number | boolean>;
-          };
-      withCredentials?: boolean;
-      reportProgress?: boolean;
-      transferCache?: { includeHeaders?: string[] } | boolean;
-    } = {}
-  ): Observable<UtilisateurRead> {
-    return this.http.get<UtilisateurRead>(`/api/utilisateurs/${utiId}`, {
-      observe: 'body',
-      ...options,
-    });
-  }
-
-  /**
-   * @description Recherche des utilisateurs
-   * @param nom Nom de l'utilisateur
-   * @param prenom Nom de l'utilisateur
-   * @param email Email de l'utilisateur
-   * @param dateNaissance Age de l'utilisateur
-   * @param adresse Adresse de l'utilisateur
-   * @param actif Si l'utilisateur est actif
-   * @param profilId Profil de l'utilisateur
-   * @param typeUtilisateurCode Type d'utilisateur
-   * @returns Utilisateurs matchant les critères
-   */
-  searchUtilisateur(
-    nom?: string,
-    prenom?: string,
-    email?: string,
-    dateNaissance?: string,
-    adresse?: string,
-    actif?: boolean,
-    profilId?: number,
-    typeUtilisateurCode?: TypeUtilisateurCode,
-    options: {
-      headers?: HttpHeaders | { [header: string]: string | string[] };
-      context?: HttpContext;
-      params?:
-        | HttpParams
-        | {
-            [param: string]:
-              | string
-              | number
-              | boolean
-              | ReadonlyArray<string | number | boolean>;
-          };
-      withCredentials?: boolean;
-      reportProgress?: boolean;
-      transferCache?: { includeHeaders?: string[] } | boolean;
-    } = {}
-  ): Observable<UtilisateurItem[]> {
-    const addParam = (key: string, value: any) => {
-      if (value !== null && value !== undefined) {
-        if (options.params instanceof HttpParams) {
-          options.params = options.params.append(key, value);
-        } else {
-          if (!options.params) {
-            options.params = {};
-          }
-          options.params[key] = value;
-        }
-      }
-    };
-    addParam('nom', nom);
-    addParam('prenom', prenom);
-    addParam('email', email);
-    addParam('dateNaissance', dateNaissance);
-    addParam('adresse', adresse);
-    addParam('actif', actif);
-    addParam('profilId', profilId);
-    addParam('typeUtilisateurCode', typeUtilisateurCode);
-
-    return this.http.get<UtilisateurItem[]>(`/api/utilisateurs`, {
-      observe: 'body',
-      ...options,
-    });
-  }
-
-  /**
-   * @description Sauvegarde un utilisateur
-   * @param utiId Id de l'utilisateur
-   * @param utilisateur Utilisateur à sauvegarder
-   * @returns Utilisateur sauvegardé
-   */
-  updateUtilisateur(
-    utiId: number,
-    utilisateur: UtilisateurWrite,
-    options: {
-      headers?: HttpHeaders | { [header: string]: string | string[] };
-      context?: HttpContext;
-      params?:
-        | HttpParams
-        | {
-            [param: string]:
-              | string
-              | number
-              | boolean
-              | ReadonlyArray<string | number | boolean>;
-          };
-      withCredentials?: boolean;
-      reportProgress?: boolean;
-      transferCache?: { includeHeaders?: string[] } | boolean;
-    } = {}
-  ): Observable<UtilisateurRead> {
-    return this.http.put<UtilisateurRead>(
-      `/api/utilisateurs/${utiId}`,
-      utilisateur,
-      { observe: 'body', ...options }
-    );
-  }
-
-  /**
-   * @description Upload de la photo d'un utilisateur
-   * @param utiId Id de l'utilisateur
-   * @param file Fichier de la photo
-   */
-  uploadPicture(
-    utiId: number,
-    file: File,
-    options: {
-      headers?: HttpHeaders | { [header: string]: string | string[] };
-      context?: HttpContext;
-      params?:
-        | HttpParams
-        | {
-            [param: string]:
-              | string
-              | number
-              | boolean
-              | ReadonlyArray<string | number | boolean>;
-          };
-      withCredentials?: boolean;
-      reportProgress?: boolean;
-      transferCache?: { includeHeaders?: string[] } | boolean;
-    } = {}
-  ): Observable<void> {
-    const body = new FormData();
-    this.fillFormData(
-      {
-        file,
-      },
-      body
-    );
-    return this.http.post<void>(`/api/utilisateurs/${utiId}/picture`, body, {
-      observe: 'body',
-      ...options,
-    });
-  }
-
-  private fillFormData(data: any, formData: FormData, prefix = '') {
-    if (Array.isArray(data)) {
-      for (const [i, item] of data.entries()) {
-        this.fillFormData(
-          item,
-          formData,
-          prefix +
-            (typeof item === 'object' && !(item instanceof File)
-              ? `[${i}]`
-              : '')
-        );
-      }
-    } else if (typeof data === 'object' && !(data instanceof File)) {
-      for (const key in data) {
-        this.fillFormData(
-          data[key],
-          formData,
-          (prefix ? `${prefix}.` : '') + key
-        );
-      }
-    } else {
-      formData.append(prefix, data);
+    /**
+     * @description Ajoute un utilisateur
+     * @param utilisateur Utilisateur à sauvegarder
+     * @returns Utilisateur sauvegardé
+     */
+    addUtilisateur(utilisateur: UtilisateurWrite, options: {headers?: HttpHeaders | {[header: string]: string | string[]}; context?: HttpContext; params?: HttpParams | {[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>}; withCredentials?: boolean; reportProgress?: boolean; transferCache?: {includeHeaders?: string[]} | boolean} = {}): Observable<UtilisateurRead> {
+        return this.http.post<UtilisateurRead>(`/api/utilisateurs`, utilisateur, {observe: 'body', ...options});
     }
-  }
+
+    /**
+     * @description Supprime un utilisateur
+     * @param utiId Id de l'utilisateur
+     */
+    deleteUtilisateur(utiId: number, options: {headers?: HttpHeaders | {[header: string]: string | string[]}; context?: HttpContext; params?: HttpParams | {[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>}; withCredentials?: boolean; reportProgress?: boolean; transferCache?: {includeHeaders?: string[]} | boolean} = {}): Observable<void> {
+        return this.http.delete<void>(`/api/utilisateurs/${utiId}`, {observe: 'body', ...options});
+    }
+
+    /**
+     * @description Download de la photo d'un utilisateur
+     * @param utiId Id de l'utilisateur
+     * @returns Fichier de la photo
+     */
+    downloadPicture(utiId: number, options: {headers?: HttpHeaders | {[header: string]: string | string[]}; context?: HttpContext; params?: HttpParams | {[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>}; withCredentials?: boolean; reportProgress?: boolean; transferCache?: {includeHeaders?: string[]} | boolean} = {}): Observable<Blob> {
+        return this.http.get(`/api/utilisateurs/${utiId}/picture`, {responseType: "blob", observe: 'body', ...options});
+    }
+
+    /**
+     * @description Charge le détail d'un utilisateur
+     * @param utiId Id de l'utilisateur
+     * @returns Le détail de l'utilisateur
+     */
+    getUtilisateur(utiId: number, options: {headers?: HttpHeaders | {[header: string]: string | string[]}; context?: HttpContext; params?: HttpParams | {[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>}; withCredentials?: boolean; reportProgress?: boolean; transferCache?: {includeHeaders?: string[]} | boolean} = {}): Observable<UtilisateurRead> {
+        return this.http.get<UtilisateurRead>(`/api/utilisateurs/${utiId}`, {observe: 'body', ...options});
+    }
+
+    /**
+     * @description Recherche des utilisateurs
+     * @param nom Nom de l'utilisateur
+     * @param prenom Nom de l'utilisateur
+     * @param email Email de l'utilisateur
+     * @param dateNaissance Age de l'utilisateur
+     * @param adresse Adresse de l'utilisateur
+     * @param actif Si l'utilisateur est actif
+     * @param profilId Profil de l'utilisateur
+     * @param typeUtilisateurCode Type d'utilisateur
+     * @returns Utilisateurs matchant les critères
+     */
+    searchUtilisateur(nom?: string, prenom?: string, email?: string, dateNaissance?: string, adresse?: string, actif?: boolean, profilId?: number, typeUtilisateurCode?: TypeUtilisateurCode, options: {headers?: HttpHeaders | {[header: string]: string | string[]}; context?: HttpContext; params?: HttpParams | {[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>}; withCredentials?: boolean; reportProgress?: boolean; transferCache?: {includeHeaders?: string[]} | boolean} = {}): Observable<UtilisateurItem[]> {
+        const addParam = (key: string, value: any) => {
+          if (value !== null && value !== undefined) {
+            if (options.params instanceof HttpParams) {
+              options.params = options.params.append(key, value);
+            } else {
+              if (!options.params) {
+                options.params = {};
+              }
+              options.params[key] = value;
+            }
+          }
+        };
+        addParam('nom', nom);
+        addParam('prenom', prenom);
+        addParam('email', email);
+        addParam('dateNaissance', dateNaissance);
+        addParam('adresse', adresse);
+        addParam('actif', actif);
+        addParam('profilId', profilId);
+        addParam('typeUtilisateurCode', typeUtilisateurCode);
+
+        return this.http.get<UtilisateurItem[]>(`/api/utilisateurs`, {observe: 'body', ...options});
+    }
+
+    /**
+     * @description Sauvegarde un utilisateur
+     * @param utiId Id de l'utilisateur
+     * @param utilisateur Utilisateur à sauvegarder
+     * @returns Utilisateur sauvegardé
+     */
+    updateUtilisateur(utiId: number, utilisateur: UtilisateurWrite, options: {headers?: HttpHeaders | {[header: string]: string | string[]}; context?: HttpContext; params?: HttpParams | {[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>}; withCredentials?: boolean; reportProgress?: boolean; transferCache?: {includeHeaders?: string[]} | boolean} = {}): Observable<UtilisateurRead> {
+        return this.http.put<UtilisateurRead>(`/api/utilisateurs/${utiId}`, utilisateur, {observe: 'body', ...options});
+    }
+
+    /**
+     * @description Upload de la photo d'un utilisateur
+     * @param utiId Id de l'utilisateur
+     * @param file Fichier de la photo
+     */
+    uploadPicture(utiId: number, file: File, options: {headers?: HttpHeaders | {[header: string]: string | string[]}; context?: HttpContext; params?: HttpParams | {[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>}; withCredentials?: boolean; reportProgress?: boolean; transferCache?: {includeHeaders?: string[]} | boolean} = {}): Observable<void> {
+        const body = new FormData();
+        this.fillFormData(
+            {
+                file
+            },
+            body
+        );
+        return this.http.post<void>(`/api/utilisateurs/${utiId}/picture`, body, {observe: 'body', ...options});
+    }
+
+    private fillFormData(data: any, formData: FormData, prefix = "") {
+        if (Array.isArray(data)) {
+            for (const [i, item] of data.entries()) {
+                this.fillFormData(item, formData, prefix + (typeof item === "object" && !(item instanceof File) ? `[${i}]` : ""));
+            }
+        } else if (typeof data === "object" && !(data instanceof File)) {
+            for (const key in data) {
+                this.fillFormData(data[key], formData, (prefix ? `${prefix}.` : "") + key);
+            }
+        } else {
+            formData.append(prefix, data);
+        }
+    }
 }
