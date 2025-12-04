24bc18fdbcdd54a9f7a791842159109838d9dd3e
Sébastien EZRATTY <sebastien.ezratty@kleegroup.com>
2025-12-04 23:01:45 +0100

    Clean code.

--- TopModel.LanguageServer/CodeActionHandler.cs
+++ TopModel.LanguageServer/CodeActionHandler.cs
@@ -61,7 +61,6 @@ public class CodeActionHandler(
                         break;
                     case ErrorType.TMD0003:
                         codeActions.AddRange(GetCodeActionCreateDomain(request, diagnostic));
-                        // TODO
                         break;
                     case ErrorType.TMD0005:
                         codeActions.AddRange(GetCodeActionMissingDecoratorImport(request, diagnostic, modelFile));
@@ -103,6 +102,49 @@ public class CodeActionHandler(
                 CodeActionKind.QuickFix,
             },
         };
+    }
+
+    protected IEnumerable<CommandOrCodeAction> GetCodeActionAddAnnotation(
+        CodeActionParams request,
+        Diagnostic diagnostic,
+        ModelFile modelFile
+    )
+    {
+        var text = modelFileCache.GetFile(request.TextDocument.Uri.GetFileSystemPath());
+        var line = text[diagnostic.Range.Start.Line];
+        var annotationName = line[diagnostic.Range.Start.Character..Math.Min(diagnostic.Range.End.Character, line.Length)];
+        return
+        [
+            new CodeAction
+            {
+                Title = $"TopModel : Créer l'annotation {annotationName} dans ce fichier",
+                Kind = CodeActionKind.QuickFix,
+                IsPreferred = true,
+                Diagnostics = new List<Diagnostic> { diagnostic },
+                Edit = new WorkspaceEdit
+                {
+                    Changes = new Dictionary<DocumentUri, IEnumerable<TextEdit>>
+                    {
+                        [new Uri(facade.GetFilePath(modelFile))] =
+                        [
+                            new()
+                            {
+                                NewText =
+                                    @$"
+---
+annotation: 
+  name: {annotationName}
+  description: 
+  target:
+    - 
+",
+                                Range = new Range(text.Length, 0, text.Length, 0),
+                            },
+                        ],
+                    },
+                },
+            },
+        ];
     }
 
     protected IEnumerable<CommandOrCodeAction> GetCodeActionAddClass(
@@ -192,49 +234,6 @@ domain:
                     };
             })
             .ToList();
-    }
-
-    protected IEnumerable<CommandOrCodeAction> GetCodeActionAddAnnotation(
-        CodeActionParams request,
-        Diagnostic diagnostic,
-        ModelFile modelFile
-    )
-    {
-        var text = modelFileCache.GetFile(request.TextDocument.Uri.GetFileSystemPath());
-        var line = text[diagnostic.Range.Start.Line];
-        var annotationName = line[diagnostic.Range.Start.Character..Math.Min(diagnostic.Range.End.Character, line.Length)];
-        return
-        [
-            new CodeAction
-            {
-                Title = $"TopModel : Créer l'annotation {annotationName} dans ce fichier",
-                Kind = CodeActionKind.QuickFix,
-                IsPreferred = true,
-                Diagnostics = new List<Diagnostic> { diagnostic },
-                Edit = new WorkspaceEdit
-                {
-                    Changes = new Dictionary<DocumentUri, IEnumerable<TextEdit>>
-                    {
-                        [new Uri(facade.GetFilePath(modelFile))] =
-                        [
-                            new()
-                            {
-                                NewText =
-                                    @$"
----
-annotation: 
-  name: {annotationName}
-  description: 
-  target:
-    - 
-",
-                                Range = new Range(text.Length, 0, text.Length, 0),
-                            },
-                        ],
-                    },
-                },
-            },
-        ];
     }
 
     protected CommandOrCodeAction GetCodeActionIgnoreWarning(
