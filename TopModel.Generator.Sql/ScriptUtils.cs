6c9589e995a7e9568ee3965eacb7b1880990bdf2
Gildéric <gilderic.deruette@kleegroup.com>
2026-02-23 16:33:55 +0100

    [SQL] Ne pas générer de colonne pour la reverse d'une oneToOne

--- TopModel.Generator.Sql/ScriptUtils.cs
+++ TopModel.Generator.Sql/ScriptUtils.cs
@@ -13,7 +13,12 @@ public static class ScriptUtils
 
     public static IEnumerable<IProperty> GetAllProperties(this Class classe, IEnumerable<Class> availableClasses)
     {
-        foreach (var prop in classe.Properties.Where(p => !p.AssociationToMany))
+        foreach (
+            var prop in classe.Properties.Where(p =>
+                !p.AssociationToMany
+                && !(p.IsReverseProperty && p is AssociationProperty { Type: AssociationType.OneToOne })
+            )
+        )
         {
             yield return prop;
         }
