0662d2b6b4791f4329e522540ad047935e8c25ab
Gildéric <gilderic.deruette@kleegroup.com>
2026-04-20 18:55:13 +0200

    [Doc] Fix edit this page link

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           routeBasePath: "/",
           editUrl:
-            "https://github.com/klee-contrib/topmodel/edit/develop/docs/docs/",
+            "https://github.com/klee-contrib/topmodel/edit/develop/docs/",
         },
         blog: false,
         theme: {
