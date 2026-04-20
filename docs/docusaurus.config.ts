ddeb80c322c9c9a0ecce84573da6606f2ef57230
Gildéric <gilderic.deruette@kleegroup.com>
2026-04-20 22:39:44 +0200

    [Doc] Changement méthode surcharge style annotation java

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import { darkTheme, lightTheme } from "./src/css/theme";
 
 const config: Config = {
   title: "TopModel",
@@ -142,8 +143,8 @@ const config: Config = {
       ],
     },
     prism: {
-      theme: prismThemes.oneLight,
-      darkTheme: prismThemes.oneDark,
+      theme: lightTheme,
+      darkTheme: darkTheme,
       additionalLanguages: ["bash", "csharp", "java", "sql", "typescript"],
     },
   } satisfies Preset.ThemeConfig,
